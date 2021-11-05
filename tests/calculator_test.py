06955b7c85f319972ed1aa8e82efe60d2020547c
svoishnis <sav27@njit.edu>
2021-11-05 17:27:20 -0400

    Added in Unit Test for Addition

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,30 +1,24 @@
 """Testing the Calculator"""
-from calculator.main import Calculator
 
-def test_calculator_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
+import unittest
+from calculator.Calculator import Calculator
+from CsvReader.csvReader import CsvReader
 
-def test_calculator_add():
-    """Testing the Add function of the calculator"""
-    calc = Calculator()
-    calc.add_number(4)
-    assert calc.result == 4
+class UnitTest(unittest.TestCase): #Test Driven Development - testing code works
+    def setUp(self) -> None:
+        self.calculator = Calculator() #Initialize Calculator in each unit test
 
-def test_calculator_subtract():
-    calc = Calculator()
-    calc.subtract_number(4)
-    assert calc.result == 4
+    def test_calculator_instance(self):
+        self.assertIsInstance(self.calculator, Calculator) #Test Existence of Calculator
+        print("Calculator Instance Test Completed")
 
-def test_calculator_multiply():
-    """ tests multiplication of two numbers"""
-    calc = Calculator()
-    calc.multiple_numbers(4)
-    assert calc.result == 4
+    def test_calculator_result(self):
+        self.assertEqual(self.calculator.result,0)
+        print("Calculator Result Test Completed")
 
-def test_calculator_divide():
-    """ tests division of two numbers"""
-    calc = Calculator()
-    calc.divide_numbers(4)
-    assert calc.result == 4
+    def test_calculator_addition(self):
+        print("Addition Test:")
+        test_data_addition = CsvReader('tests/Data/UnitTest_Addition.csv')
+        for row in test_data_addition:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']),int(row['Result']))
+            print("Addition Test Complete")
