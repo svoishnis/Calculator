488f043d7267bd23b9bc291b98549ff5d07a9697
svoishnis <sav27@njit.edu>
2021-11-06 12:12:20 -0400

    Rebase Project with Default setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='calculator',
-      version='0.0.1',
-      description="Calculator Assignment for CS601",
-      author="Sarah Voishnis",
-      author_email="sav27@njit.edu",
-      url="",
-      packages=find_packages(),
-      )
+setup(name="calculator", packages=find_packages())
