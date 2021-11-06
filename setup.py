1b6a52d9b42421128796ec38fd056cce5cadbabb
svoishnis <sav27@njit.edu>
2021-11-05 21:07:26 -0400

    Import package fix and set up file spacing

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='Calculator',
-      version = '0.0.1',
-      description = "Calculator Assignment for CS601",
-      author = "Sarah Voishnis",
-      author_email = "sav27@njit.edu",
-      url = "",
-      , packages=find_packages(),
-)
+setup(name='calculator',
+      version='0.0.1',
+      description="Calculator Assignment for CS601",
+      author="Sarah Voishnis",
+      author_email="sav27@njit.edu",
+      url="",
+      packages=find_packages(),
+      )
