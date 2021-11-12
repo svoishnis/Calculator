4c2d78a906e25d6bf03534bc0c004db75fb03d5b
svois <sav27@njit.edu>
2021-11-12 15:57:44 -0500

    Get the clear history fixure class to work FINALLLLLLLLLYYYYYY!!

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -8,21 +8,9 @@ class Calculator:
     history = []
 
     @staticmethod
-    def get_result_of_last_calculation_added_to_history():
-        """gets the last item added to the list"""
-        return Calculator.history[-1].getresult()
-
-
-    @staticmethod
-    def add_calculation_to_history(calculation):
-        """Adds current item to the list"""
-        Calculator.history.append(calculation)
-        return True
-
-    @staticmethod
     def get_result_of_first_calculation_added_to_history():
         """Gets the First item from the list"""
-        return Calculator.history[0].getResult()
+        return Calculator.history[0].getresult()
 
     @staticmethod
     def clear_history():
@@ -34,6 +22,17 @@ class Calculator:
     def history_count():
         """Returns the amount of Calculations Stored"""
         return len(Calculator.history)
+
+    @staticmethod
+    def add_calculation_to_history(calculation):
+        """Adds current item to the list"""
+        Calculator.history.append(calculation)
+        return True
+
+    @staticmethod
+    def get_result_of_last_calculation_added_to_history():
+        """gets the last item added to the list"""
+        return Calculator.history[-1].getresult()
 
     @staticmethod
     def add_number(value_a, value_b):
@@ -61,5 +60,3 @@ class Calculator:
         if value_b == 0:
             raise ZeroDivisionError("Cannot divide by zero")
         return value_a / value_b
-
-
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,12 +2,13 @@
 import pprint
 import pytest
 from calculator.calculator import Calculator
+
 @pytest.fixture
-def clear_history():
-    """This method is created for Unit Testing Only"""
+def _clear_history():
+    """Required don't delete"""
     Calculator.clear_history()
 
-def test_calculator_add(clear_history):
+def test_calculator_add(_clear_history): # pylint: disable=unused-argument
     """Testing the Add function of the calculator"""
     assert Calculator.add_number(1, 2) == 3
     assert Calculator.add_number(2, 2) == 4
@@ -17,7 +18,7 @@ def test_calculator_add(clear_history):
     assert Calculator.get_result_of_last_calculation_added_to_history() == 6
     pprint.pprint(Calculator.history)
 
-def test_clear_history(clear_history):
+def test_clear_history(_clear_history): # pylint: disable=unused-argument
     """This tests the clearing history method defined above"""
     assert Calculator.add_number(1,2) == 3
     assert Calculator.add_number(2, 2) == 4
@@ -27,19 +28,38 @@ def test_clear_history(clear_history):
     assert Calculator.clear_history()
     assert Calculator.history_count() == 0
 
-def test_calculator_subtract(clear_history):
+def test_count_history(_clear_history): # pylint: disable=unused-argument
+    """Testing the count of history method"""
+    assert Calculator.history_count() == 0
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.history_count() == 2
+
+def test_get_last_calculation_result(_clear_history): # pylint: disable=unused-argument
+    """Tests the method to get that result of the last calculation"""
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 5
+
+def test_get_first_calculation_result(_clear_history): # pylint: disable=unused-argument
+    """Tests the method to retrieve the result fo the first calculation"""
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.get_result_of_first_calculation_added_to_history() == 4
+
+def test_calculator_subtract(_clear_history): # pylint: disable=unused-argument
     """Testing the subtract method of the calculator"""
     assert Calculator.subtract_number(1, 2) == -1
 
-def test_calculator_multiply(clear_history):
+def test_calculator_multiply(_clear_history): # pylint: disable=unused-argument
     """ tests multiplication of two numbers"""
     assert Calculator.multiply_numbers(1,2) == 2
 
-def test_calculator_divide(clear_history):
+def test_calculator_divide(_clear_history): # pylint: disable=unused-argument
     """ tests multiplication of two numbers"""
     assert Calculator.divide_numbers(2,3) == 0.6666666666666666
 
-def test_calculator_divide_except(clear_history):
+def test_calculator_divide_except(_clear_history): # pylint: disable=unused-argument
     """ tests the exception for the division of two numbers"""
     try:
         assert Calculator.divide_numbers(3, 0)
