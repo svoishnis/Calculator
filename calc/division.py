d502a6fdcbc559d113fd4b801124634d5ef70081
svois <sav27@njit.edu>
2021-11-13 10:05:30 -0500

    100% coverage - check not needed

--- calc/division.py
+++ calc/division.py
@@ -6,6 +6,4 @@ class Division(Calculation):
     """Division class has one method- get the result of calculation from parent class"""
     def getresult(self):
         """This is encapsulation example"""
-        if self.value_b == 0:
-            raise ZeroDivisionError
         return self.value_a / self.value_b
