ac4f7c4815c6fb38f37b8702dced0b5c6a033b93
svois <sav27@njit.edu>
2021-11-15 18:49:35 -0500

    Update to calculations fix errors

--- calc/history/calculations.py
+++ calc/history/calculations.py
@@ -1,25 +1,30 @@
 """Calculation history Class"""
 class Calculations:
+    """Create a storage for calculation history"""
     history = []
     # pylint: disable=too-few-public-methods
     @staticmethod
     def clear_history():
+        """clears history of calculations"""
         Calculations.history.clear()
         return True
     @staticmethod
     def count_history():
+        """Method returns the amount of calculations stored"""
         return len(Calculations.history)
     @staticmethod
     def get_last_calculation():
+        """Returns the most recent calculation stored"""
         return Calculations.history[-1]
     @staticmethod
     def get_first_calculation():
+        """Returns the oldest calculation stored"""
         return Calculations.history[-1]
     @staticmethod
     def get_calculation(num):
-        """ get a specific calculation from history"""
+        """Returns a specific calculation stored"""
         return Calculations.history[num]
     @staticmethod
     def add_calculation(calculation):
-        """ get a specific calculation from history"""
+        """ Add a specific calculation to the history"""
         return Calculations.history.append(calculation)
