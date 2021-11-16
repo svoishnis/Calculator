18708815e53fa662510dbb136793ab2188af8592
svois <sav27@njit.edu>
2021-11-15 19:17:42 -0500

    First set of fixes for Calculator

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -22,14 +22,13 @@ class Calculator:
         return True
     @staticmethod
     def get_result_of_last_calculation_added_to_history():
-        # -1 gets the last item added to the list automaticly and you can expect it to have the get result method
+        # method gets the last item added to the list automatically
         return Calculator.history[-1].getResult()
     @staticmethod
     def add_number(value_a, value_b):
         """ adds number to result"""
         #create an addition object using the factory we created on the calculation class
         addition = Addition.create(value_a,value_b)
-        # addition = Addition(value_a,value_b) <-this is not good but will work.  It will be repeated too much
         Calculator.add_calculation_to_history(addition)
         return Calculator.get_result_of_last_calculation_added_to_history()
     @staticmethod
@@ -38,12 +37,10 @@ class Calculator:
         """ subtract number from result"""
         # create an subtraction object using the factory we created on the calculation class
         subtraction = Subtraction.create(value_a, value_b)
-        # addition = Addition(value_a,value_b) <-this is not good but will work.  It will be repeated too much
         Calculator.add_calculation_to_history(subtraction)
         return Calculator.get_result_of_last_calculation_added_to_history()
     @staticmethod
     def multiply_numbers(value_a, value_b):
         """ multiply two numbers and store the result"""
-        #this is a shorthand way to create the multiplication object and added it the history in one line
         Calculator.add_calculation_to_history(Multiplication.create(value_a,value_b))
         return Calculator.get_result_of_last_calculation_added_to_history()
