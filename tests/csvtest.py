baf472827ab6e980906eabc4f6f8f280d7448e20
svois <sav27@njit.edu>
2021-11-30 18:25:12 -0500

    Update naming due to warnings

--- tests/csvtest.py
+++ tests/csvtest.py
@@ -1,9 +1,11 @@
+"""Testing the CSV Methods"""
 import pytest
 
-from calc.CSVReader.main import getAbbPath
+from calc.csvreader.main import getAbbPath
 
 @pytest.fixture
 def test_csv_get_abb_path():
+    """Test for getting the absolute path"""
     file = 'addition_test.py'
     filename = getAbbPath(file)
     return filename
