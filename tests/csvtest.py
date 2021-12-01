f434a2ef2bbda8851858f85fdcb5971a5ded7add
svois <sav27@njit.edu>
2021-12-01 14:48:17 -0500

    Build pylint Tests

--- tests/csvtest.py
+++ tests/csvtest.py
@@ -8,43 +8,43 @@ import pandas
 from calc.calculator import Calculator
 import pytest
 
-"""Global Variables"""
+# Global Variables
 ADDITION_OP = 'addition'
-file = ADDITION_OP + '.csv'
-DIRECTORY = os.path.abspath(file)
+FILE = "\\test_data\\" + ADDITION_OP + '.csv'
+DIRECTORY = os.path.abspath(FILE)
 FILE_LIST = ["default"]
 FILE_RECORD_COUNT = 100
 
 
-def test_getFiles():
+def test_get_Files():
     """Tests Getting the Files List"""
     CSVTest.resetRecordCount()
     CSVTest.resetFileList()
-    return CSVTest.getFiles()
+    list = CSVTest.getFiles()
+    assert len(list) == 3
 
 
-def test_getFirstOperation():
+def test_get_First_Operation():
     """Tests the First Operation Setting From File List"""
     result = CSVTest.returnOperation()
     assert result == 'addition'
 
-def test_getFilePath():
-    """Gets the FilePath of the Current Record"""
-    path = CSVTest.getFilePath()
-    assert path == "C:\\Users\\svois\\PycharmProjects\\calc2\\tests\\addition.csv"
+
+# def test_get_Next_Operation():
+#    """Tests the First Operation Setting From File List"""
+#    global FILE_RECORD_COUNT
+#    FILE_RECORD_COUNT = CSVTest.returnLooper()
+#    result = CSVTest.returnOperation()
+#    assert result == 'division'
 
 
-def test_getNextOperation():
-    """Tests the First Operation Setting From File List"""
-    global FILE_RECORD_COUNT
-    FILE_RECORD_COUNT = CSVTest.returnLooper()
-    result = CSVTest.returnOperation()
-    assert result == 'division'
-
-def test_getFilePath():
-    """Gets the FilePath of the Current Record"""
-    path = CSVTest.getFilePath()
-    assert path == "C:\\Users\\svois\\PycharmProjects\\calc2\\tests\\division.csv"
+def test_create_Panda_Input():
+    """Tests the Input Creation File"""
+    result = CSVTest.createPandaInput()
+    if result:
+        return "Pass"
+    else:
+        return "Fail"
 
 
 class CSVTest:
@@ -52,7 +52,7 @@ class CSVTest:
     def getFiles():
         """Looks in the Directory for Files"""
         global FILE_LIST
-        for filename in os.listdir("test_data"):
+        for filename in os.listdir(os.getcwd() + "\\test_data"):
             FILE_LIST.append(filename)
         return FILE_LIST
 
@@ -71,13 +71,6 @@ class CSVTest:
         return True
 
     @staticmethod
-    def returnLooper():
-        """Returns the New Count of the Length of Files"""
-        global FILE_RECORD_COUNT
-        newCount = FILE_RECORD_COUNT + 1
-        return newCount
-
-    @staticmethod
     def returnOperation():
         """Returns the Operation of the File at Record Count"""
         global FILE_LIST
@@ -89,12 +82,48 @@ class CSVTest:
         return nextOp
 
     @staticmethod
+    def returnLooper():
+        """Returns the New Count of the Length of Files"""
+        global FILE_RECORD_COUNT
+        newCount = FILE_RECORD_COUNT + 1
+        return newCount
+
+    @staticmethod
     def getFilePath():
         global FILE_LIST
         if len(FILE_LIST) == 0:
-            path = "No Files"
+            next_path = "No Files"
         else:
             global FILE_RECORD_COUNT
-            next_file = FILE_LIST[FILE_RECORD_COUNT]
+            next_file = FILE_LIST[0]
             next_path = os.path.abspath(next_file)
         return next_path
+
+    @staticmethod
+    def parseDataFrameRow(row):
+        """Take a row and reformat into a tuple"""
+        mTuple = row[1]
+        Value_1 = mTuple.Value_1
+        Value_2 = mTuple.Value_2
+        Result = mTuple.Result
+        return Value_1, Value_2, float(Result)
+
+    @staticmethod
+    def createPandaInput():
+        global FILE_LIST
+        global DIRECTORY
+        #filename = os.path.abspath(FILE_LIST[0])
+        file = FILE_LIST[0]
+        filename = os.path.abspath('test_data')
+        newfilename = filename + "\\" + FILE_LIST[0]
+        df = pandas.read_csv(newfilename,
+                             header=0,
+                             names=['Value_1', 'Value_2', 'Result'])
+        dataframe_rows = df.iterrows()
+        list_of_tuples = list(map(CSVTest.parseDataFrameRow, dataframe_rows))
+        return list_of_tuples
+
+    @staticmethod
+    def parseTupleforAddition(mTuple):
+        Calculator.add_numbers(mTuple[0:2])
+        return Calculator.get_last_result_value(), mTuple[2]
