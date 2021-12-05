4344d8d0c1c590eec924cb6ef4d943964d8a6da9
svois <sav27@njit.edu>
2021-12-05 13:04:24 -0500

    Subtraction calculation fix

--- calc/calculations/subtraction.py
+++ calc/calculations/subtraction.py
@@ -10,7 +10,9 @@ class Subtraction(Calculation):
     def get_result(self):
         """get the subtraction results"""
         difference_of_values = 0.0
-        for value in self.values:
-            difference_of_values = difference_of_values - value
+        for value in self.values[:1]:
+            difference_of_values = value
+        for value in self.values[1:]:
+            difference_of_values = difference_of_values + (-1 * value)
             pprint.pprint(value)
         return difference_of_values
