2b770de06ad9b5d2716d8cfcbac2fabd5b66e489
svois <sav27@njit.edu>
2021-12-14 01:03:22 -0500

    Beautiful Tables Image

--- app/controllers/csv_calc_controller.py
+++ app/controllers/csv_calc_controller.py
@@ -2,6 +2,8 @@ import csv
 import os
 from flask import render_template, request
 from werkzeug.utils import secure_filename
+
+from calc.calculator import Calculator
 from calc.testesttest import CsvPostLogic
 from app.controllers.controller import ControllerBase
 
@@ -29,9 +31,25 @@ class CsvCalcController(ControllerBase):
             reader = csv.reader(csvfile, delimiter=',')
             next(reader)
             data = [tuple(line) for line in reader]
+            calc_list = []
+            validation_list = []
+            for value1, value2, result in data:
+                my_tuple1 = (value1, value2)
+                calc = Calculator.addition(my_tuple1)
+                if calc == result:
+                    result = "Pass"
+                else:
+                    result = "Fail"
+
+                my_tuple2 = (Calculator.get_last_result_value(), result)
+
+                # calc_list.append(str(Calculator.get_last_result_value()))
+
+
 
         # asdfasdf = CsvPostLogic()
         # kdksk = asdfasdf.testtest()
         # list_of_tuples = list((kdksk,kdksk,kdksk ))
 
-        return render_template('csv_calc_result.html', success=success, operation=operation, rows=data)
+        return render_template('csv_calc_result.html', success=success, operation=operation, rows=data,
+                               validation=my_tuple2)
